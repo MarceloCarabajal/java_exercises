06fc934342b0af7cc5d0721451457ac6427f2360
MarceloCarabajal <marce.tsgia@gmail.com>
2024-01-03 16:14:59 -0300

    arreglo de conflictos

--- Conversiones/src/main/java/org/example/Main.java
+++ Conversiones/src/main/java/org/example/Main.java
@@ -2,6 +2,40 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("---------- Conversion De Double a Int y Double----------");
+        double num = 1.78;
+
+        //casteo a entero
+        int numInt = (int) num;
+
+        //casteo a long
+        long numLong = (long) num;
+
+        System.out.println("double: "+ num);
+        System.out.println("int: " + numInt);
+        System.out.println("long: " + numLong);
+
+        System.out.println("----------De String a Int y Double----------");
+
+        String cantidad = "15";
+        String precio = "150.27";
+
+        int cantInt = Integer.parseInt(cantidad);
+        double precioDouble = Double.parseDouble(precio);
+
+        double total = cantInt * precioDouble;
+        System.out.println("El valor total de la compra es: " + total);
+
+        System.out.println("----------De Int/Double a String----------");
+
+        int edad = 33;
+        double estatura = 1.80;
+
+        String edadString = String.valueOf(edad);
+        String estaturaString = String.valueOf(estatura);
+
+        System.out.println("Edad: " + edadString);
+        System.out.println("Estatura: " + estaturaString);
+
     }
 }
